7fb0186a20f204375e55d234fe1842647802d130
WilliamJm <willjm.wj@gmail.com>
2026-04-18 20:21:24 -0500

    Implementacion de API REST

--- README.md
+++ README.md
@@ -19,3 +19,13 @@
 
 ## Creacion de Front simple
 !['Creación del repositorio'](./docs/front.png)
+
+## Implementación de API REST (Django REST Framework)
+
+## Consumo de la API
+
+!['API Root'](./docs/api_root.png)
+
+## Listado de Movies (GET)
+
+!['Listado de Movies'](./docs/api_movies.png)
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 from .models import Movie
+from .serializers import MovieSerializer
 
-def movie_list(request):
-    movies = Movie.objects.all()
-    return render(request, 'movies/movie_list.html', {'movies': movies})
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
